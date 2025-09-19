689929edd05f9c1c9aa347d883522483a51995c1
Francis <francisabainza714@gmail.com>
2025-09-19 23:36:33 +0800

    Add cors middleware

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+app.use(cors({ origin: ["http://localhost:3000", "https://last-minute-learner.vercel.app"] }));
 
 app.get("/api/custom", (req, res) => {
   res.json({ message: "Hello from Express API" });
 });
 
-// Render will give you a port in process.env.PORT
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`API server running on port ${port}`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`API server listening on ${port}`));
